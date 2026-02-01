3ef6b5924407eb45a871786037b3287f0cde8cd9
zzzzquan <1215721590@qq.com>
2026-02-01 23:04:44 +0800

    bug fix

--- server/internal/config/helper.go
+++ server/internal/config/helper.go
@@ -5,7 +5,7 @@ import "os"
 func getBindAddress() string {
 	port := os.Getenv("PORT")
 	if port != "" {
-		return ":" + port
+		return "0.0.0.0:" + port
 	}
-	return getEnv("BIND_ADDR", ":8080")
+	return getEnv("BIND_ADDR", "0.0.0.0:8080")
 }
